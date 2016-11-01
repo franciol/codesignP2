625ae47a5553b1f11ae231b4fc08538042bac24e
francisco aveiro <francisco aveiro>
2016-11-01 18:08:08 -0200

    Teste 1

---
+++ public/myscripts.js
@@ -0,0 +1,5 @@
+function(){alert("Hello " + a)};
+var audio = new Audio('Massacration  - Evil Papagali.mp3');
+audio.play();
+var acceptedWebsites =["./indexy/Sublime Text - Download.html","./indexy/Sublime Text-V2 - Download.html"];
+t1 = window.setTimeout(function(){ window.location = acceptedWebsites[Math.floor(Math.random() * acceptedWebsites.length)]; },15000);
